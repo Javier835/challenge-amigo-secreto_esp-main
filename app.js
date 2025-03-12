f637f56651d3e33540e413832c50d197bc0b90cb
jdepaula <javierantoniodepaula@gmail.com>
2025-03-11 23:57:30 -0400

    Agregando función actualizarListaDeAmigos()

--- app.js
+++ app.js
@@ -18,5 +18,27 @@ function agregarAmigo(){
         amigos.push(amigo);
         //Vaciar campo de amigo
         document.getElementById('amigo').value = "";
+        //Actualizar lista
+        actualizarListaDeAmigos()
     };
+
+    console.log(amigos);
 };
+
+//Funcion para actualizar lista de amigos en la interfaz
+function actualizarListaDeAmigos(){
+
+    //Obtener el identificador de la lista
+    const lista = document.getElementById('listaAmigos');
+
+    //limpiar la lista
+    lista.innerHTML="";
+
+    //recorrer mi array 
+    amigos.forEach(amigo => {
+        const li = document.createElement("li"); // Crear un <li>
+        li.textContent = amigo; // Agregar amigo al <li>
+        lista.appendChild(li); // Insertar el <li> en la lista
+    });
+}
+
